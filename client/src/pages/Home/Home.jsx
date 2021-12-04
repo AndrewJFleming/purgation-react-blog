2b13216368a6f48fc5ffb8a92f6d97069cfe3ccb
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2021-12-03 16:38:19 -0800

    Create Posts and Post components and import in Home component
    -Create Posts component and import into Home component.
    -Create dummy post objects state in Home component and pass as props to Posts component.
    -Create Posts component, import into Posts component and map out Posts. -Pass post props down to Post component and populate Post content dynamically.
    -Style home wrapper as display: flex so sidebar and posts sit side-by-side.

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Posts from "../../components/Posts/Posts";
 import Header from "../../components/Header/Header";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import "./Home.css";
 
 export default function Home() {
+  const [posts, setPosts] = useState([
+    {
+      photo:
+        "https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "Test Post 1",
+      _id: "1",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate qui necessitatibus nostrum illum reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate qui necessitatibus nostrum illum reprehenderit.",
+      categories: [
+        { name: "testCat1" },
+        { name: "testCat2" },
+        { name: "testCat3" },
+      ],
+    },
+    {
+      photo:
+        "https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "Test Post 2",
+      _id: "2",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate qui necessitatibus nostrum illum reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate qui necessitatibus nostrum illum reprehenderit.",
+      categories: [
+        { name: "testCat1" },
+        { name: "testCat2" },
+        { name: "testCat3" },
+      ],
+    },
+    {
+      photo:
+        "https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "Test Post 3",
+      _id: "3",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate qui necessitatibus nostrum illum reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate qui necessitatibus nostrum illum reprehenderit.",
+      categories: [
+        { name: "testCat1" },
+        { name: "testCat2" },
+        { name: "testCat3" },
+      ],
+    },
+  ]);
+
+  useEffect(() => {}, []);
+
   return (
     <div>
       <Header />
-      <Sidebar />
+      <div className="home">
+        <Posts posts={posts} />
+        <Sidebar />
+      </div>
     </div>
   );
 }
