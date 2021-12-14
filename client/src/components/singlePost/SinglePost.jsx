f6f43c21f4a9d6c72f2266949bca37e5c0be0464
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2021-12-13 16:07:32 -0800

    Replace add cat code in SinglePost.jsx with a render of AddCategories component.

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import "./SinglePost.css";
 import { Context } from "../../shared/context/Context";
 import { ErrorPrompt } from "../../shared/components/ErrorPrompt/ErrorPrompt";
+import { AddCategories } from "../../shared/components/AddCategories/AddCategories";
 
 export default function SinglePost() {
   const location = useLocation();
@@ -19,9 +20,6 @@ export default function SinglePost() {
   const [error, setError] = useState(false);
   const { user } = useContext(Context);
 
-  const [newCategory, setNewCategory] = useState([]);
-  const [existsPrompt, setExistsPrompt] = useState(false);
-
   useEffect(() => {
     const getPost = async () => {
       const res = await axios.get("/posts/" + path);
@@ -33,27 +31,6 @@ export default function SinglePost() {
     };
     getPost();
   }, [path]);
-
-  const handlePush = () => {
-    const exists = categories.includes(newCategory);
-    if (!exists && newCategory) {
-      setCategories((categories) => [...categories, newCategory]);
-      setNewCategory("");
-    } else if (!newCategory) {
-      console.log("blank");
-    } else {
-      setExistsPrompt(true);
-      setTimeout(() => {
-        setExistsPrompt(false);
-      }, 3000);
-      setNewCategory("");
-    }
-  };
-
-  const handleRemove = (targetCat) => {
-    const newArray = categories.filter((item) => item !== targetCat);
-    setCategories(newArray);
-  };
 
   const handleDelete = async () => {
     setError(false);
@@ -142,48 +119,10 @@ export default function SinglePost() {
               value={description}
               onChange={(e) => setDescription(e.target.value)}
             />
-            <div className="writeFormGroup">
-              <h3>Categories</h3>
-              <div className="addCatWrapper">
-                <div className="addCatLeft">
-                  <input
-                    type="text"
-                    placeholder="Category name"
-                    className="addCatInput"
-                    value={newCategory}
-                    autoFocus={true}
-                    onChange={(e) => setNewCategory(e.target.value)}
-                  />
-                  <span
-                    className="addCat"
-                    onClick={() => handlePush(newCategory)}
-                  >
-                    Add
-                  </span>
-                  {existsPrompt && (
-                    <p className="existsPrompt">Already exists...</p>
-                  )}
-                </div>
-                <div className="addCatRight">
-                  <ul className="catList">
-                    <h4>Categories to be added:</h4>
-                    {categories.length ? (
-                      categories.map((cat) => (
-                        <li>
-                          <i
-                            className="writeCatIcon far fa-trash-alt"
-                            onClick={() => handleRemove(cat)}
-                          ></i>
-                          <label>{cat}</label>
-                        </li>
-                      ))
-                    ) : (
-                      <p className="noCats">Nothing added yet...</p>
-                    )}
-                  </ul>
-                </div>
-              </div>
-            </div>
+            <AddCategories
+              categories={categories}
+              setCategories={setCategories}
+            />
           </React.Fragment>
         ) : (
           <React.Fragment>
