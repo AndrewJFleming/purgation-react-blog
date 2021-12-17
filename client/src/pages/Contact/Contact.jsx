e3b12bd3c1ee7464a255eaa792da9f0833ba885b
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2021-12-17 14:30:01 -0800

    Make Contact component's labels serif

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -46,7 +46,7 @@ export default function Contact() {
           </p>
           <form className="contactForm" onSubmit={handleSubmit}>
             <div className="contactFormGroup">
-              <label>Subject</label>
+              <h5 className="serifTitle">Subject</h5>
               <input
                 type="text"
                 placeholder="Subject"
@@ -56,7 +56,7 @@ export default function Contact() {
             </div>
             <div className="contactFormGroup d-flex justify-content-between">
               <div className="inputWrapper">
-                <label>Name</label>
+                <h5 className="serifTitle mt-3">Name</h5>
                 <input
                   type="text"
                   placeholder="Name"
@@ -65,7 +65,7 @@ export default function Contact() {
                 />
               </div>
               <div className="inputWrapper">
-                <label>Email</label>
+                <h5 className="serifTitle mt-3">Email</h5>
                 <input
                   type="text"
                   placeholder="Email"
@@ -75,7 +75,7 @@ export default function Contact() {
               </div>
             </div>
             <div className="contactFormGroup">
-              <label>Message</label>
+              <h5 className="serifTitle mt-3">Message</h5>
               <textarea
                 placeholder="Message..."
                 type="text"
